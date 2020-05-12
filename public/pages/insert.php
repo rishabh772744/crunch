e6a240f172a9d4f2405cf3ce24e1963bdcf06f5e
Prajneya <prajneya.kumar@research.iiit.ac.in>
2020-05-12 14:06:19 +0530

    Add UI for all pages

---
+++ public/pages/insert.php
@@ -0,0 +1,58 @@
+<?php 
+
+	
+
+	$name = $_POST['name'];
+	$userid = $_POST['userid'];
+	$email = $_POST['email'];
+	$mno = $_POST['mno'];
+	$rcheck = $_POST['rcheck'];
+	$tid = $_POST['tid'];
+	
+	if(!empty($name) || !empty($userid) || !empty($email) || !empty($mno) || !empty($rcheck)){
+		$host = "localhost";
+		$dbusername = "ofgtdepi_sce";
+		$dbpassword = "6MXMv1wHSgj0R";
+		$dbname = "ofgtdepi_sce";
+
+		//creating connection
+		$conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+		if(mysqli_connect_error()){
+			die('Connect Error('. mysqli_connect_errorno().')'. mysqli_connect_error());
+		}
+		else{
+			$SELECT = "SELECT email from registrations where email = ? limit 1";
+			$INSERT = "INSERT into registrations (name, userid, email, mno, rcheck, tid) values(?,?,?,?,?,?)";
+
+			//prepare statement
+			$stmt = $conn->prepare($SELECT);
+			$stmt->bind_param("s", $email);
+			$stmt->execute();
+			$stmt->bind_result($email);
+			$stmt->store_result();
+
+			$rnum = $stmt->num_rows;
+
+			if(rnum==0){
+				$stmt->close();
+
+				$stmt = $conn->prepare($INSERT);
+				$stmt->bind_param("sssiss", $name, $userid, $email, $mno, $rcheck, $tid);
+				$stmt->execute();
+				echo '<script type="text/javascript">
+           window.location = "http://crunched.in/pages/success.html"
+      </script>';
+			}
+			else{
+				echo "All fields are required";
+			}
+			$stmt->close();
+			$conn->close();
+		}
+	}
+	else{
+		echo "All fields are required";
+		die();
+	}
+ ?>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,338 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="no-js">
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  <head>
+  <!-- <meta name="viewport" content="width=device-width, initial-scale=1" /> -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Crunch - HOME</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+      <!-- Bootstrap -->
+      <link rel="stylesheet" href="{{ url('assets/styles/vendor/bootstrap.min.css') }}">
+      <!-- Fonts -->
+      <link rel="stylesheet" href="{{ url('assets/fonts/et-lineicons/css/style.css') }}">
+      <link rel="stylesheet" href="{{ url('assets/fonts/linea-font/css/linea-font.css') }}">
+      <link rel="stylesheet" href="{{ url('assets/fonts/fontawesome/css/font-awesome.min.css') }}">
+      <!-- Slider -->
+      <link rel="stylesheet" href="{{ url('assets/styles/vendor/slick.css') }}">
+      <!-- Lightbox -->
+      <link rel="stylesheet" href="{{ url('assets/styles/vendor/magnific-popup.css') }}">
+      <!-- Animate.css -->
+      <link rel="stylesheet" href="{{ url('assets/styles/vendor/animate.css') }}">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
 
-            .position-ref {
-                position: relative;
-            }
+      <!-- Definity CSS -->
+      <link rel="stylesheet" href="{{ url('assets/styles/main.css') }}">
+      <link rel="stylesheet" href="{{ url('assets/styles/responsive.css') }}">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+      <!-- JS -->
+      <script src="{{ url('assets/js/vendor/modernizr-2.8.3.min.js') }}"></script>
+  </head>
+  <body id="page-top" data-spy="scroll" data-target=".navbar">
+      <!-- Google Tag Manager -->
+      <noscript><iframe src="http://www.googletagmanager.com/ns.html?id=GTM-MH7TSF"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      '../../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MH7TSF');</script>
+      <!-- End Google Tag Manager -->
 
-            .content {
-                text-align: center;
-            }
+      <!--[if lt IE 8]>
+          <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+      <![endif]-->
 
-            .title {
-                font-size: 84px;
-            }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+      <!-- ========== Preloader ========== -->
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+      <div class="preloader">
+        <img src="assets/images/loader.svg" alt="Loading...">
+      </div>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+
+      <!-- ========== Navigation ========== -->
+
+      <nav class="navbar navbar-default navbar-fixed-top navbar-inverse navbar-trans  navbar-fw" role="navigation">
+        <div class="navbar-header page-scroll">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- Logo -->
+
         </div>
-    </body>
+
+        <!-- Navbar Links -->
+        <div id="navbar" class="navbar-collapse collapse page-scroll navbar-right">
+          <ul class="nav navbar-nav">
+            <li><a href="#home">Home<span class="sr-only"></span></a></li>
+            <li><a href="#about">About<span class="sr-only"></span></a></li>
+            <li><a href="{{ url('pages/schedule.html') }}">Schedules & Registrations<span class="sr-only"></span></a></li>
+            <li><a href="{{ url('pages/instruction.html') }}">Instructions & T&C<span class="sr-only"></span></a></li>
+            <li><a href="{{ url('pages/contact.html') }}">Contact<span class="sr-only"></span></a></li>
+
+          </ul><!-- / .nav .navbar-nav -->
+        </div><!--/.navbar-collapse -->
+      </nav><!-- / .navbar -->
+
+
+
+      <!-- ========== Hero Cover ========== -->
+
+      <div id="home" class="agency2-hero">
+        <div class="bg-overlay">
+
+          <div id="large-header" class="large-header">
+            <canvas id="demo-canvas" class="demo-canvas"></canvas>
+
+            <!-- Hero Content -->
+            <div class="hero-content-wrapper">
+              <div class="hero-content">
+
+
+                <h1 class="hero-lead">Game.</h1>
+                <h1 class="hero-lead">Win. Repeat.</h1>
+
+                <h4 class="h-alt hero-secondary" style="margin-left: 25%; margin-right: 25%;" >
+                  <font color="white"><b>The best place to play PUBG Mobile     win exiting Hampers and Real Cash Prizes</b></font>
+                </h4>
+
+              </div><!-- / .hero-content -->
+            </div><!-- / .hero-content-wrapper -->
+
+          </div><!-- / #large-header -->
+
+        </div><!-- / .bg-overlay -->
+      </div><!-- / #home -->
+
+
+
+      <!-- ========== About - Section ========== -->
+
+      <section id="about" class="container">
+        <div class="row section">
+
+          <header class="sec-heading">
+            <h2>WELCOME TO CRUNCH</h2>
+            <span class="subheading"><font color="red">You think you are the best PUBG player out there? But so do 99 others. This is your chance to prove them wrong.</font></span>
+          </header>
+
+          <div>
+            <blockquote>Welcome to Crunch. Here, at Crunch, our aim is to find the best gamers in India, and show them the country's love and support. We look forward to finding gamers who would represent India at Global Gaming Events. Are you the one?
+            <p><p>
+            To move forward with this, as our first step, we are launching PUBG Mobile Tournaments, which will be held everday according to their respective schedules. These tournaments have an entry fee, and the winners get amazing cash prizes ranging from 2000.00 INR to 25000.00 INR, alongwith cool merchandise and hamper packs to show off their gaming style.
+            <p>
+            So crunch your hands up and get ready to show your gaming skills to the rest of the world.
+          </div>
+
+          <div class="col-md-12 text-center">
+            <a href="{{ url('pages/prizes.html') }}" class="btn-text">PRIZES</a>
+            <a href="{{ url('pages/schedule.html') }}" class="btn-text">SCHEDULE & REGISTRATIONS</a>
+          </div>
+        </div><!-- / .row -->
+
+      </section><!-- / .container -->
+
+
+
+      <!-- ========== Number Counters - Dark bg ========== -->
+
+      <div class="number-counters-dark-bg">
+        <div class="container">
+          <div id="counters-min" class="row count-wrapper">
+  <h4><font color="white" style="margin-left: 5%;"> OUR NEXT TOURNAMENT IS: PMQS ( PUBG MOBILE QUORA SERIES ) </font></h4>
+  <br> <h4><font color="white" style="margin-left: 5%;"> DATES TO BE DECIDED SOON </font></h4>
+  <br> <a href="pages/pmqs.html" class="btn btn-light btn-large wow fadeIn" data-wow-delay=".3s" data-wow-duration="1s" style="margin-left: 50rem;">MORE DETAILS</a>
+          </div><!-- / .row  -->
+        </div><!-- / .container -->
+      </div><!-- / .number-counters
+
+
+
+
+      <!-- ========== Feature - Steps Numbers ========== -->
+
+      <section class="container ft-steps-numbers">
+          <div class="row section">
+
+            <header class="sec-heading ws-s">
+              <h2>Registration process</h2>
+              <span class="subheading">Three Simple Steps</span>
+            </header>
+
+            <!-- Step 1 -->
+            <div class="col-lg-4 col-md-6 mb-sm-100 ft-item wow fadeIn" data-wow-duration="1s">
+              <span class="ft-nbr">01</span>
+              <h4>Register</h4>
+              <p>Go to the Schedules & Registration Page and Register yourself for the respective match by paying the entry fee and providing the neccessary details. Do not forget to read the Instructions & T&C Page.</p>
+            </div>
+
+            <!-- Step 2 -->
+            <div class="col-lg-4 col-md-6 mb-sm-100 ft-item wow fadeIn" data-wow-duration="1s" data-wow-delay=".3s">
+              <span class="ft-nbr">02</span>
+              <h4>Strategise</h4>
+              <p>Get your strategies for the game ready. You will need them to win the best of the prizes. In the meanwhile, we will be contacting you and providing you the ROOM ID & PASSWORD for the game. You will be added to a whatsapp group/notified by email, as per your choice, which you will be providing on the Registrations Page.</p>
+            </div>
+
+            <!-- Step 3 -->
+            <div class="col-lg-4 col-md-6 ft-item wow fadeIn" data-wow-duration="1s" data-wow-delay=".6s">
+              <span class="ft-nbr">03</span>
+              <h4>Deploy</h4>
+              <p>That's it! Welcome to the club! All you need to do now is play, and win!</p>
+            </div>
+
+          </div><!-- / .row -->
+
+          <!-- CTA Button -->
+          <div class="row ws-m">
+            <div class="text-center">
+              <a href="{{ url('pages/instruction.html') }}" class="btn">Read Instructions & T&C</a>
+            </div>
+          </div><!-- / .row -->
+
+      </section><!-- / .container -->
+
+
+
+      <!-- ========== CTA - Contact Link ========== -->
+
+      <div class="cta-link cta-dark">
+        <div class="bg-overlay">
+
+          <div class="cta-wrapper">
+            <h3 class="h-alt wow fadeIn" data-wow-delay=".1s" data-wow-duration="1s">See what we can do for you</h3>
+            <a href="{{ url('pages/contact.html') }}" class="btn btn-light btn-large wow fadeIn" data-wow-delay=".3s" data-wow-duration="1s">Get in touch</a>
+          </div>
+
+        </div>
+      </div><!-- / .dark-bg .cta-link -->
+
+
+
+      <!-- ========== Footer Contact ========== -->
+
+      <footer id="contact" class="footer-contact">
+        <div class="container-fluid">
+          <div class="row">
+
+            <!-- Map and address -->
+            <div class="col-lg-6 no-gap contact-info">
+
+              <!-- Show Info Button -->
+              <a href="#" class="show-info-link"><i class="fa fa-info"></i>Show info</a>
+
+              <div id="" class="footer-map"></div>
+
+              <address class="contact-info-wrapper">
+                <ul>
+                  <!-- Address -->
+
+                  <!-- Email -->
+                  <li class="contact-group">
+                    <span class="adr-heading">Email</span>
+                    <span class="adr-info">rishabh772744@gmail.com</span>
+                  </li>
+                </ul>
+                <ul>
+                  <!-- Phone -->
+
+                  <!-- Mobile -->
+
+                </ul>
+
+
+              </address>
+
+            </div><!-- / .col-lg-6 -->
+
+
+            <!-- Contact Form -->
+            <div class="col-lg-6 no-gap section contact-form">
+              <header class="sec-heading">
+                <h2>Contact</h2>
+                <span class="subheading">Send your message directly to us.</span>
+              </header>
+
+
+            </div><!-- / .col-lg-6 -->
+
+          </div><!-- / .row -->
+        </div><!-- / .container-fluid -->
+
+
+        <!-- Social Links -->
+        <div class="dark-bg">
+          <div class="container footer-social-links">
+            <div class="row">
+
+              <ul>
+                <li><a href="#">facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="https://www.instagram.com/_crunched/">Instagram</a></li>
+              </ul>
+
+            </div>
+          </div><!-- / .container -->
+        </div><!-- / .dark-bg -->
+
+
+        <!-- Copyright -->
+        <div class="copyright">
+          <div class="container">
+            <div class="row">
+
+              <div class="col-sm-6">
+                <small>&copy; 2018 <a class="no-style-link" href="" target="_blank">CRUNCHED.</a></small>
+              </div>
+
+              <div class="col-md-6">
+                <small><a href="#page-top" class="pull-right to-the-top">To the top<i class="fa fa-angle-up"></i></a></small>
+              </div>
+
+            </div><!-- / .row -->
+          </div><!-- / .container -->
+        </div><!-- / .copyright -->
+      </footer><!-- / .footer-contact -->
+
+
+
+      <!-- ========== Scripts ========== -->
+
+      <script src="{{ url('assets/js/vendor/jquery-2.1.4.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/google-fonts.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.easing.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.waypoints.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/bootstrap.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/bootstrap-hover-dropdown.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/smoothscroll.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.localScroll.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.scrollTo.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.stellar.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.parallax.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/slick.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.easypiechart.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/countup.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/isotope.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.magnific-popup.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/wow.min.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/jquery.ajaxchimp.js') }}"></script>
+      <script src="{{ url('assets/js/vendor/animDots.js') }}"></script>
+      <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+      <!-- Google Maps -->
+      <script src="{{ url('assets/js/gmap.js') }}"></script>
+      <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDOcd7o0W0r846FC_GoHSK56xeAvP8fV4s"></script>
+
+      <!-- Definity JS -->
+      <script src="{{ url('assets/js/main.js') }}"></script>
+  </body>
 </html>
